91baca32fd88bf33bc88c2435ed82d98321a69d3
Jaime Tyler <jaimetyler88@gmail.com>
2019-03-16 00:31:07 -0400

    doing some psuedo-coding and setting up html.

---
+++ assets/Javascript/app.js
@@ -0,0 +1,28 @@
+console.log("do it!")
+
+//make a variable for the starting time
+
+//make a timer.
+
+//make a variable to keep track of what number question user is on
+
+//make a variable to keep track of the number of correct answers to call at the end of the game
+
+//make a variable to keep track of the number of incorrect answers
+
+//create objects that container the question, choices and correct answer/image for each question.(the image may come from somewhere else.need to look that up).
+
+//make a button click event that will trigger a start game function.
+
+// make a function that will start the game that will start a timer and load a new question with choices into the html. 
+
+// make a function that will display "That's Right! with an image and also increase the correct answers count by 1.
+
+//make a function that will display "Incorrect! the correct answer is...insert the correct answeer" with a wrong answer image and also increase the incorrect answers count by 1. 
+
+//make a function that will take you to the next question and restart the timer
+
+//make a function that will reset the game without reloading the page
+
+
+
